f23faf63d1899656a9ffcb70c45ef0a0a9fa437f
evy-goryunov <evy.goryunov@gmail.com>
2019-05-24 14:01:22 +0300

    1

---
+++ Guess_The_Number/Guess_The_Number/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Guess_The_Number
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TitleLbl = new System.Windows.Forms.Label();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnStart = new System.Windows.Forms.Button();
+			this.QuestLable = new System.Windows.Forms.Label();
+			this.GuessText = new System.Windows.Forms.TextBox();
+			this.btnGuess = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// TitleLbl
+			// 
+			this.TitleLbl.AutoSize = true;
+			this.TitleLbl.Font = new System.Drawing.Font("MS Outlook", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.TitleLbl.Location = new System.Drawing.Point(27, 25);
+			this.TitleLbl.Name = "TitleLbl";
+			this.TitleLbl.Size = new System.Drawing.Size(68, 25);
+			this.TitleLbl.TabIndex = 0;
+			this.TitleLbl.Text = "label1";
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(496, 288);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 2;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnStart
+			// 
+			this.btnStart.Location = new System.Drawing.Point(496, 27);
+			this.btnStart.Name = "btnStart";
+			this.btnStart.Size = new System.Drawing.Size(75, 23);
+			this.btnStart.TabIndex = 3;
+			this.btnStart.Text = "Start";
+			this.btnStart.UseVisualStyleBackColor = true;
+			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+			// 
+			// QuestLable
+			// 
+			this.QuestLable.AutoSize = true;
+			this.QuestLable.Location = new System.Drawing.Point(32, 70);
+			this.QuestLable.Name = "QuestLable";
+			this.QuestLable.Size = new System.Drawing.Size(35, 13);
+			this.QuestLable.TabIndex = 4;
+			this.QuestLable.Text = "label1";
+			// 
+			// GuessText
+			// 
+			this.GuessText.Location = new System.Drawing.Point(32, 110);
+			this.GuessText.Name = "GuessText";
+			this.GuessText.Size = new System.Drawing.Size(35, 20);
+			this.GuessText.TabIndex = 5;
+			this.GuessText.TextChanged += new System.EventHandler(this.GuessText_TextChanged);
+			// 
+			// btnGuess
+			// 
+			this.btnGuess.Location = new System.Drawing.Point(496, 70);
+			this.btnGuess.Name = "btnGuess";
+			this.btnGuess.Size = new System.Drawing.Size(75, 23);
+			this.btnGuess.TabIndex = 6;
+			this.btnGuess.Text = "Guess";
+			this.btnGuess.UseVisualStyleBackColor = true;
+			this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(603, 350);
+			this.Controls.Add(this.btnGuess);
+			this.Controls.Add(this.GuessText);
+			this.Controls.Add(this.QuestLable);
+			this.Controls.Add(this.btnStart);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.TitleLbl);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "Guess The Number";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label TitleLbl;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Button btnStart;
+		private System.Windows.Forms.Label QuestLable;
+		private System.Windows.Forms.TextBox GuessText;
+		private System.Windows.Forms.Button btnGuess;
+	}
+}
+
---
+++ Guess_The_Number/Guess_The_Number/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess_The_Number
+{
+	public partial class Form1 : Form
+	{
+		int questNumber;
+		public Form1()
+		{
+			
+			InitializeComponent();
+			btnStart.Text = "Start";
+			TitleLbl.Text = "Guess The Number Game";
+			QuestLable.Visible = false;
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void btnStart_Click(object sender, EventArgs e)
+		{
+			Random r = new Random();
+			questNumber = r.Next(0, 100);
+			QuestLable.Text = "Угадайте число от 0 до 100";
+			QuestLable.Visible = true;
+			TitleLbl.Text = questNumber.ToString();
+		}
+
+		private void GuessText_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnGuess_Click(object sender, EventArgs e)
+		{
+			if (int.Parse(GuessText.Text) == questNumber)
+			{
+				QuestLable.Text = "Вы угадали!";
+			}
+		}
+	}
+}
